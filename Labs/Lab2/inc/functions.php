a6d7ada0d69953495b9b01de16cd1ca23ff2e43f
Cory Heads <cheads@csumb.edu>
2017-09-10 05:06:14 +0000

    Best I could do

--- Labs/Lab2/inc/functions.php
+++ Labs/Lab2/inc/functions.php
@@ -1,33 +1,36 @@
 <?php
 
 
- function displaySymbol($randomNumber, $pos){
+ function displaySymbol($randomNumber){
             
          
-
-
+        
         if ($randomNumber == 0){
             $symbol = "seven";
         } else if ($randomNumber == 1) {
             $symbol ="cherry";
+        } else if ($randomNumber == 2) {
+            $symbol ="grapes";
         } else {
             $symbol = "lemon";
         }
         
         
-        echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='$symbol'width='70'/>";
+        echo "<img src='img/$symbol.png' alt='$symbol' title='$symbol'width='70'/>";
         }
         
-        function displayPoints($randomNumber1, $randomNumber2, $randomNumber3){
+        function displayPoints($randomNumber1, $randomNumber2, $randomNumber3, $randomNumber4){
             echo"<div id = 'output'>";
-            if ($randomNumber1 == $randomNumber2 && $randomNumber2 == $randomNumber3) {
+            if ($randomNumber1 == $randomNumber2 && $randomNumber2 == $randomNumber3 && $randomNumber2 == $randomNumber4) {
                 switch ($randomNumber1) {
                     case 0: $totalPoints = 1000;
                     echo "<h1> JACKPOT!!! <h1>";
                     break;
-                    case 1: $totalPoints = 500;
+                    case 1: $totalPoints = 900;
                     break;
-                    case 2: $totalPoints = 250;
+                    case 2: $totalPoints = 500;
+                    break;
+                    case 3: $totalPoints = 250;
                     break;
                 }
                 echo "<h2> You won $totalPoints points! </h2";
@@ -35,25 +38,24 @@
             
                 echo "<h1> TRY AGAIN!</h1>";
             }
-                echo "</div>";
-              for ($i=1; $i<4; $i++){
-                        ${"randomValue" . $i } = rand (0,2);
-                        displaySymbol(${"randomValue" . $i}, $i );
+            echo "</div>";
+        
         }
-        }
-        $randomNumber1 = rand(0,2);
+        $randomNumber1 = rand(0,3);
         
         
         displaySymbol($randomNumber1);
-        $randomNumber2 = rand(0,2);
+        $randomNumber2 = rand(0,3);
         displaySymbol($randomNumber2);
-        $randomNumber3 = rand(0,2);
+        $randomNumber3 = rand(0,3);
         displaySymbol($randomNumber3);
+        $randomNumber3 = rand(0,3);
+        displaySymbol($randomNumber4);
         
         echo "<br>The random values are: <br>";
-        echo $randomNumber1 . " , "  . $randomNumber2 .  " & "  . $randomNumber3;
+        echo $randomNumber1 . " , "  . $randomNumber2 .  " , "  . $randomNumber3 .  " & "  . $randomNumber4;
         
-        displayPoints($randomNumber1, $randomNumber2, $randomNumber3);
+        displayPoints($randomNumber1, $randomNumber2, $randomNumber3, $randomNumber4);
         function play (){
             
         }
--- Labs/Lab2/index.php
+++ Labs/Lab2/index.php
@@ -4,17 +4,15 @@
         <title> 777 Slot Machine </title>
         <meta charset="utf-8" />
         
-          <link rel="stylesheet" type="text/css" href="style.css">
+        
         
     </head>
     <body>
-        <div id-"main">
+        <div id="main">
         <?php
        include 'inc/functions.php';
         ?>
-        <img id='reel1' src='img/lemon.png' alt='lemon' title='Lemon' width='70' >
-<img id='reel2' src='img/cherry.png' alt='cherry' title='Cherry' width='70' >
-<img id='reel3' src='img/lemon.png' alt='lemon' title='Lemon' width='70' >
+ 
 
       
         <form>
