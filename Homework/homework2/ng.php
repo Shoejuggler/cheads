d50828cfd4f369909fd58235b514ffb0da20395b
Cory Heads <cheads@csumb.edu>
2017-09-24 03:54:44 +0000

    cards

--- Homework/homework2/ng.php
+++ Homework/homework2/ng.php
@@ -1,4 +1,25 @@
+<?php
 
+$result;
+
+$names = array('Christopher','Ryan','Ethan','John','Zoey','Sarah','Michelle','Samantha','Walker','Thompson', 'Anderson','Johnson','Tremblay','Peltier', 'Cunningham', 'Simpson', 'Mercado', 'Sellers', 'Ryan');
+
+
+
+
+function generateName($names){
+    global $result;
+    $result = $firstName[$names].'  ' . $lastName[$names];
+    echo "Witness Protection has assigned you the new identety of: $result";
+    
+
+
+}
+for ($i=0; $i< 1; $i++) {
+ $rand = rand(0,20);
+ generateName($result);
+}
+?>
 
 
 
@@ -13,28 +34,7 @@
     <div id="cornerz">
     <div class="head">
 <?php
-
-$names = array('Christopher','Ryan','Ethan','John','Zoey','Sarah','Michelle','Samantha',);
-
-$surnames = array('Walker','Thompson', 'Anderson','Johnson','Tremblay','Peltier', 'Cunningham', 'Simpson', 'Mercado', 'Sellers', 'Ryan');
-
-function randomGenerator($random_name, $random_surname){
-$random_name = $names;
-$random_surname = $surnames;
-
-
-if ($names == $surnames){
-   randomGenerator();
-} else{
- echo $random_name . ' ' . $random_surname;
-}
-
-
-}
-for ($i=0; $i< 1; $i++) {
-${"rand" . $i}= rand(0, 4);
-}
-print randomGenerator();
+echo generateName();
 ?>
 </div>
     </body>
--- Labs/Lab3/cards.php
+++ Labs/Lab3/cards.php
@@ -3,38 +3,81 @@
         for ($i=0; $i=52; $i++){
             $deck[$i]= $i;
         }*/
-        $deck = range (1,52);
+        $deck = range (1,41);
         shuffle($deck);
-        print_r($deck);
+        //print_r($deck);
+        $totalPoints=0;
+        
         
         function displayHand() {
-            global $deck;
+            global $deck, $totalPoints;
+            $handPoints = 0;
+            $handAces = 0;
             for ($i = 0 ; $i < 5; $i++) {
                 //echo $deck[$i] . ' ';
                 $lastCard = array_pop($deck);
-                echo $lastCard % 13 . " ";
+                $cardValue = $lastCard % 13;
+                $cardSuit;
+                if($lastCard <= 13) {
+                    $cardSuit = "clubs";
+                }
+                else if($lastCard > 13 && $lastCard <= 26){
+                    $cardSuit = "diamonds";}
+                else if($lastCard > 26 && $lastCard <= 39){
+                    $cardSuit = "hearts";}
+                else if($lastCard > 39){
+                    $cardSuit = "spades";}
+                    if ($cardValue == 0) {
+                        $cardValue = 13;
+                    }
+                if($lastCard > 13 && $lastCard);{
+                }
+                
+                    if ($cardValue == 1){
+                        echo "<img class = 'ace' img src='cards/$cardSuit/$cardValue.png' alt ='Ace' />";
+                        $handAces = $handAces +1;   //$handAces++;
+                    }
+                
+                //echo $lastCard % 13 . " ";
+                $handPoints = $handPoints + $cardValue;
+                //$handPoints += $cardValue
+                
+                    echo "<img src='cards/$cardSuit/$cardValue.png' alt ='Ace' />";
             }
+            echo " Points: " . $handPoints;
+            
+            echo " Aces: " . $handAces;
+            
+            $totalPoints = $totalPoints + $handPoints;
+            
+            return  $handAces;
         }
-        echo "<hr>";
-        displayHand();
-        echo "<hr>";
-        displayHand();
-        echo "<hr>";
+        
+
         
         
         
         
         
-        function displayRandomCard(){
-        $randomCard = rand(1,13);
+        /*function displayRandomCard(){
+        //$randomCard = rand(1,13);
         $cardSuites = array("diamonds", "hearts", "spades", "clubs");
-        $randomSuite = rand (0,3);
+       // $randomSuite = rand (0,3);
 echo "<img src='cards/$cardSuites[$randomSuite]/$randomCard.png'/>";
 
 }
 for($i=0; $i<5; $i++){
-    displayRandomCard();
-}
+    
+}*/
+function identifyWinner($player1Aces, $computerAces){
+    if ($player1Aces > $computerAces) {
+    echo "Player One";
+    }else if ($player1Aces < $computerAces) {
+        echo "Computer";
+    }else
+        echo "No one";
+    }
+
 
 ?>
 
@@ -44,10 +87,34 @@ for($i=0; $i<5; $i++){
 <html>
     <head>
         <title>Lab 3: Ace Poker </title>
+        <style>
+            .ace{
+                border: yellow 3px solid;
+            }
+        </style>
     </head>
     <body>
+<header> 
+<h1> Ace Poker</h1>
+<br>
+<h2>Hand with the most aces wins!</h2>
+</header>
+<?php
+$player1Aces = displayHand();
+?>
+<br>
+<?php
+$computerAces = displayHand();
+?>
+<h2>
+<?php
+identifyWinner($player1Aces, $computerAces)
+?>
 
-
+Wins:
+<?=$totalPoints?>
+ Points!
+</h2>
 
     </body>
 </html>
