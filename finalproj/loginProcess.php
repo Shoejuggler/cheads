22913381e169109a70d793ca23c754d0b80fcb4d
Cory Heads <cheads@csumb.edu>
2017-12-13 19:32:14 +0000

    fix

--- finalproj/loginProcess.php
+++ finalproj/loginProcess.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-   include '../../dbConnection.php';
+   include 'dbConnection.php';
    $conn = getDatabaseConnection();
 $username = $_POST['username'];
 $password = sha1($_POST['password']);
